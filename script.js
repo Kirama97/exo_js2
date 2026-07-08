d414b4447664391f0de09a9ec8a4dc5cfccd7fe3
Kirama97 <officialthiam97@gmail.com>
2026-07-08 13:23:32 +0000

    mon premier commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let a = parseInt(prompt('veuillez me donner un valeur entier pour A'));
+let b = parseInt(prompt('veuillez me donner un valeur entier pour B'));
+
+let c = a + b ;
+
+// ici sera afficher le resultat
+
+alert("le resultat de loperation est : " + c)
